2b9b057b31b01514e0492832dc9de4fdfbb612bf
pasqdvt <71087965+pasqdvt@users.noreply.github.com>
2021-10-26 16:41:48 +0200

    rename

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-GITHUB_REPO="pagopa/test_docker"
-IMAGE_NAME="test_docker"
+GITHUB_REPO="pasqdvt-org/ms-auth"
+IMAGE_NAME="ms-auth"
 VERSION="0.1"
 
 IMAGE_ID="ghcr.io/${GITHUB_REPO}/${IMAGE_NAME}"
